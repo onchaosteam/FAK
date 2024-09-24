62ca29b7d0215da3575c45a19f643f4a5648539f
onchaosteam <57648718+onchaosteam@users.noreply.github.com>
2024-09-25 08:26:15 +1200

    Create latecommands.sh

---
+++ latecommands.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+latecommands() {
+
+    # Modify Hosts file
+    sed -i 's/127.0.1.1/192.168.2.19/' /etc/hosts
+
+    # Add PVE Repository to install PVE Kernel package
+    echo "deb [arch=amd64] http://download.proxmox.com/debian/pve bookworm pve-no-subscription" > /etc/apt/sources.list.d/pve-install-repo.list
+    wget https://enterprise.proxmox.com/debian/proxmox-release-bookworm.gpg -O /etc/apt/trusted.gpg.d/proxmox-release-bookworm.gpg
+    apt update
+    apt install proxmox-default-kernel -y
+
+    # Download systemd component and PVE install script
+    curl -sSL 'https://raw.githubusercontent.com/onchaosteam/FAK/refs/heads/main/firstboot.sh' -o "/root/firstboot.sh"
+    curl -sSL 'https://raw.githubusercontent.com/onchaosteam/FAK/refs/heads/main/firstboot.service' -o "/etc/systemd/system/firstboot.service"
+
+    # Enable systemd to run PVE install script on boot
+    chmod 744 /root/firstboot.sh
+    chmod 664 /etc/systemd/system/firstboot.service
+    systemctl enable firstboot.service
+
+}
+
+latecommands
